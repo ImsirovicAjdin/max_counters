4b69f7dd8535b6d4d9af78723a09185536117078
Ajdin Imsirovic <ajdika@live.com>
2017-07-31 09:09:05 +0000

    write 2 tests

---
+++ counter.rb
@@ -0,0 +1,3 @@
+def solution(n, a)
+  [1, 0, 0]
+end
--- kata_spec.rb
+++ kata_spec.rb
@@ -1,5 +1,13 @@
+require_relative "counter.rb"
+
 describe "solution" do
   it "should correctly handle 1 instruction" do
-    expect(solution(3, [1]).to eq [1, 0, 0]
+    expect(solution(3, [1])).to eq [1, 0, 0]
+  end
+  
+  it "should increase the first number by 2" do
+    expect(solution(3, [1, 1])).to eq [2, 0, 0]
   end
 end
+
+
