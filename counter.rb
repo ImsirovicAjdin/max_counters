c9d177dcafbc70398431f28937c72c24f3466a34
Dmitry Maksyoma <ledestin@gmail.com>
2017-07-31 22:48:41 +1200

    Add max counter caching

--- counter.rb
+++ counter.rb
@@ -1,11 +1,13 @@
 def solution(number_of_counters, instructions)
   counters = Array.new number_of_counters, 0
+  @max_counter_value = 0
 
   instructions.each do |instruction|
     if max_counters_instruction?(instruction, number_of_counters)
-      counters.fill counters.max
+      counters.fill @max_counter_value
     else
-      counters[instruction - 1] += 1
+      new_counter_value = counters[instruction - 1] += 1
+      update_max_counter new_counter_value
     end
   end
   counters
@@ -13,4 +15,10 @@ end
 
 def max_counters_instruction?(instruction, number_of_counters)
   instruction > number_of_counters
+end
+
+def update_max_counter(new_counter_value)
+  if @max_counter_value < new_counter_value
+    @max_counter_value = new_counter_value
+  end
 end
