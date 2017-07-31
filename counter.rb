5e92c29f3a84b450155c06e5bb0c84fd1f793e16
Dmitry Maksyoma <ledestin@gmail.com>
2017-07-31 21:17:45 +1200

    Pong

--- counter.rb
+++ counter.rb
@@ -1,3 +1,8 @@
 def solution(number_of_counters, instructions)
-  [1, 0, 0]
+  counters = Array.new number_of_counters, 0
+
+  instructions.each do |instruction|
+    counters[instruction - 1] += 1
+  end
+  counters
 end
--- kata_spec.rb
+++ kata_spec.rb
@@ -8,6 +8,10 @@ describe "solution" do
   it "should increase the first number by 2" do
     expect(solution(3, [1, 1])).to eq [2, 0, 0]
   end
+
+  it "should handle 2 counters increase" do
+    expect(solution(3, [1, 3])).to eq [1, 0, 1]
+  end
 end
 
 
