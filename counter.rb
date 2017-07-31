65b00d45d998a1473a3abdf4221c50295e02334d
Dmitry Maksyoma <ledestin@gmail.com>
2017-07-31 22:23:58 +1200

    Pass the test

--- counter.rb
+++ counter.rb
@@ -2,7 +2,15 @@ def solution(number_of_counters, instructions)
   counters = Array.new number_of_counters, 0
 
   instructions.each do |instruction|
-    counters[instruction - 1] += 1
+    if max_counters_instruction?(instruction, number_of_counters)
+      counters.fill counters.max
+    else
+      counters[instruction - 1] += 1
+    end
   end
   counters
+end
+
+def max_counters_instruction?(instruction, number_of_counters)
+  instruction > number_of_counters
 end
