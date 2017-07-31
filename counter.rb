e8470137bf1f2330cd55889a395d2ef693ba5dae
Dmitry Maksyoma <ledestin@gmail.com>
2017-07-31 22:53:27 +1200

    Hash implementation

--- counter.rb
+++ counter.rb
@@ -1,16 +1,22 @@
 def solution(number_of_counters, instructions)
-  counters = Array.new number_of_counters, 0
+  counters = Hash.new 0
   @max_counter_value = 0
 
   instructions.each do |instruction|
     if max_counters_instruction?(instruction, number_of_counters)
-      counters.fill @max_counter_value
+      counters = Hash.new @max_counter_value
     else
       new_counter_value = counters[instruction - 1] += 1
       update_max_counter new_counter_value
     end
   end
-  counters
+
+  result = Array.new number_of_counters, counters.default
+  counters.each_pair do |counter, value|
+    result[counter] = value
+  end
+
+  result
 end
 
 def max_counters_instruction?(instruction, number_of_counters)
